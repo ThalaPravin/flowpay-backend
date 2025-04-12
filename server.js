f1fc2a6ab9b4167cbce7553adcd35055d3219525
Pravin <pravinavhad3597@gmail.com>
2025-04-12 10:45:46 +0530

    Route setup at Server.js

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import userRoutes from './Routes/userRoutes'
 
 dotenv.config();
 connectDB();
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('api/users', userRoute)
 const PORT = process.env.PORT
 
 app.listen(PORT, () => {
